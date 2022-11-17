3c31fe4794a6cd0c3511c1fe678adac6cd3dec61
Asvad <asvadkhadchukaev@gmail.com>
2022-11-17 22:30:58 +0300

    Second commit

--- script.py
+++ script.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import requests as rq
+from bs4 import BeautifulSoup as bs
+import scrapy
+
+url = 'https://www.kinopoisk.ru/lists/movies/top250/'
+r = rq.get(url).text
+soup = bs(r, 'lxml')
+soup.find('div', class_='styles_root__ti07r')
+#captcha problem
