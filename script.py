7299d3c3c53a8ac864da8abba3eae3790d23d30d
a-vdovenko <vdovenkosubscriptions@gmail.com>
2022-11-20 10:56:23 +0800

    add TODO comment

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as bs
 import scrapy
 
 url = 'https://www.kinopoisk.ru/lists/movies/top250/'
+# TODO: make variables more explicit r -> request
 r = rq.get(url).text
 soup = bs(r, 'lxml')
 soup.find('div', class_='styles_root__ti07r')
