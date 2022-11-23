b8322032283e4354b6c79f7ca4576df671bde623
Asvad <asvadkhadchukaev@gmail.com>
2022-11-23 22:21:35 +0300

    Second commit

--- script.py
+++ script.py
@@ -3,8 +3,29 @@ import requests as rq
 from bs4 import BeautifulSoup as bs
 import scrapy
 
-url = 'https://www.kinopoisk.ru/lists/movies/top250/'
-r = rq.get(url).text
-soup = bs(r, 'lxml')
-soup.find('div', class_='styles_root__ti07r')
-#captcha problem
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from time import sleep
+from tqdm import tqdm 
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+s = Service('/usr/bin/chromedriver')
+d = webdriver.Chrome(service=s, options=chrome_options)
+d.get('https://www.google.nl/')
+
+
+
+
+
+
