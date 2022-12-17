d84e124dc864ac5c329a3e91108619ded8b33bfc
a-vdovenko <vdovenkosubscriptions@gmail.com>
2022-12-17 23:43:27 +0800

    major main.py update, major code refactoring

--- main.py
+++ main.py
@@ -4,60 +4,49 @@ import pandas as pd
 from selenium.webdriver.common.by import By
 
 
+def find_links(parser: KinoPoisk) -> list[str]:
+
+    all_links = []
+    page = 1
+
+    while True:
+        url_of_page = parser.base_url + str(page)
+        parser.go_to_page(url_of_page)
+        logger.info(f'Went to page: {page}')
+        links = parser.find_film_links()
+        logger.info(f'Parsed {len(links)} links')
+        if links != []:
+            all_links.extend(links)
+            page += 1
+            continue
+        else:
+            break
+    logger.info(f'Total parsed links: {len(all_links)}')
+
+    return all_links
+
+
+def find_all_film_data(parser: KinoPoisk, all_links: list) -> list[dict]:
+
+    all_film_data = []
+    for link_number, link in enumerate(all_links):
+        parser.go_to_page(link)
+        film_data = parser.film_characteristics()
+        all_film_data.append(film_data)
+        logger.info(f'Got data from the {link_number+1} link film')
+
+    return all_film_data
+
+
 def main():
-    with KinoPoisk() as Parser:
-        all_links = []
-        page = 1
-        d = {}
 
-        while True:
-            url_of_page = Parser.base_url + str(page)
-            Parser.go_to_page(url_of_page)
-            logger.info(f'Went to page: {page}')
-            links = Parser.find_film_links()
-            logger.info(f'Parsed {len(links)} links')
-            if links != []:
-                all_links.extend(links)
-                page += 1
-                continue
-            else:
-                break
-        logger.info(f'Total parsed links: {len(all_links)}')
+    with KinoPoisk() as parser:
 
-        logger.info('Starting getting columns')
-        all_columns = []
-        link_number = 0
-        for link in all_links:
-            Parser.go_to_page(url=link)
-            columns = Parser.find_elements(By.CLASS_NAME, 'styles_title__b1HVo')
-            for column in columns:
-                all_columns.append(column.text)
-            link_number += 1
-            logger.info(f'Processed {link_number} films columns')
-        all_columns = list(set(all_columns))
-        logger.info('All_columns is set')
+        all_links = find_links(parser)
+        all_film_data = find_all_film_data(parser, all_links)
 
-        film = 0
-        columns = {column: [] for column in all_columns}
-        columns['Название фильма'] = []
-        for link in all_links:
-            Parser.go_to_page(link)
-            characteristics = Parser.film_characteristics()
-            film_data = {ch[0]: ch[1:2] for ch in characteristics}
-            for column in list(columns.keys()):
-                if column not in list(film_data.keys()):
-                    film_data[column] = ['Null']
-                else:
-                    continue
-            for row in film_data:
-                if columns[row] == []:
-                    columns[row] = film_data[row]
-                else:
-                    columns[row].extend(film_data[row])
-            film += 1
-            logger.info(f'Processed {film} films')
-        df = pd.DataFrame(columns)
-        df.to_csv('Kinopoisk.csv')
+        df = pd.DataFrame(all_film_data)
+        df.to_csv('KinoPoisk_2.csv', index=False)
 
 
 if __name__ == '__main__':
--- scraper.py
+++ scraper.py
@@ -4,24 +4,25 @@ from selenium.webdriver.common.by import By
 
 
 class KinoPoisk(webdriver.Chrome):
-    """Class is used for parsing and analysing data from Kinopoisk website
+    """Class is used for parsing data from Kinopoisk website
 
     Args:
-        webdriver (_type_): _description_
+        webdriver: Chrome web driver
 
     Methods:
     go_to_page()
-        Method to go to the website
+        Method to go to the website by given url
     find_film_links()
         Method to get url links to single movie
     film_characteristics()
-        Method to get data to single movie
+        Method to get data from single movie
 
     """
+
     def __init__(
         self,
-        base_url='https://www.kinopoisk.ru/lists/movies/top250/?page=',
-        teardown=False
+        base_url: str = 'https://www.kinopoisk.ru/lists/movies/top250/?page=',
+        teardown: bool = False
     ):
         self.base_url = base_url
         self.teardown = teardown
@@ -44,54 +45,100 @@ class KinoPoisk(webdriver.Chrome):
 
     def __enter__(self):
         print('Start Parsing')
+
         return self
 
     def __exit__(self, exc_type, exc_value, ex_traceback):
         print('End Parsing')
         self.quit()
 
-    def go_to_page(self, url: str):
+    def go_to_page(self, url: str) -> None:
         """Method to go to the web page through url
 
         Args:
-            url (_type_): _description_
+            url (str): Web address of page
+
+        Returns:
+            None:
         """
         self.get(url)
 
-    def find_film_links(self, class_name: str = 'base-movie-main-info_link__YwtP1') -> list[str]:
+    def find_film_links(
+        self,
+        class_name: str = 'base-movie-main-info_link__YwtP1'
+    ) -> list[str]:
         """Method to get url links to single movie from top 250 list of movies
 
         Args:
-            class_name (str, optional): _description_. Defaults to 'base-movie-main-info_link__YwtP1'.
+            class_name (str, optional): Name of movie class.
+            Defaults to 'base-movie-main-info_link__YwtP1'.
 
         Returns:
-            list[str]: _description_
+            list[str]: List with urls of movies
         """
         films = self.find_elements(By.CLASS_NAME, class_name)
         links = []
         for film in films:
             link = film.get_attribute('href')
             links.append(link)
+
         return links
 
-    def film_characteristics(self, class_name_characteristics: str ='styles_row__da_RK', class_name_title: str='styles_title__hTCAr') -> list[list]:
-        """Method to get data to single movie from top 250 list of movies
+    def film_characteristics(
+        self,
+        class_name_characteristics: str = 'styles_row__da_RK',
+        class_name_title: str = 'styles_title__hTCAr'
+    ) -> list[list]:
+        """Method to get data from single movie from top 250 list of movies
 
         Args:
-            class_name_characteristics (str, optional): _description_. Defaults to 'styles_row__da_RK'.
-            class_name_title (str, optional): _description_. Defaults to 'styles_rootInDark__SZlor'.
+            class_name_characteristics (str, optional): Name of the class of movie characteristics.
+            Defaults to 'styles_row__da_RK'.
+            class_name_title (str, optional): Name of thew class of movie name.
+            Defaults to 'styles_rootInDark__SZlor'.
 
         Returns:
-            _type_: _description_
+            list: List of lists of movie characteristics
         """
-        characteristics = self.find_elements(By.CLASS_NAME, class_name_characteristics)
+
         characteristics_text = []
+
         film_name = self.find_element(By.CLASS_NAME, class_name_title).text
-        characteristics_text.append(['Название фильма',film_name.split("\n")])
+        characteristics_text.append(['Название фильма', film_name.split("\n")])
+        characteristics = self.find_elements(
+            By.CLASS_NAME, class_name_characteristics
+        )
         for characteristic in characteristics:
             ch = characteristic.text.split("\n")
-            if '4K доступно только на больших экранах' in ch or 'Качество видео' in ch:
-                continue
-            else:
+            if ch not in ('4K доступно только на больших экранах', 'Качество видео'):
                 characteristics_text.append(ch)
+
         return characteristics_text
+
+    def film_characteristics(
+        self,
+        class_name_characteristics: str = 'styles_row__da_RK',
+        class_name_title: str = 'styles_title__hTCAr'
+    ) -> dict[str, str]:
+        """Method to get data from single movie from top 250 list of movies
+
+        Args:
+            class_name_characteristics (str, optional): Name of the class of movie characteristics.
+            Defaults to 'styles_row__da_RK'.
+            class_name_title (str, optional): Name of thew class of movie name.
+            Defaults to 'styles_rootInDark__SZlor'.
+
+        Returns:
+            dict: Data dictionary for single movie
+        """
+        film_name = self.find_element(By.CLASS_NAME, class_name_title).text
+        characteristics = self.find_elements(
+            By.CLASS_NAME, class_name_characteristics
+        )
+
+        film_data = {ch[0]: ch[1:2] for ch in (characteristic.text.split("\n") for characteristic in characteristics) if ch not in (
+            '4K доступно только на больших экранах', 'Качество видео'
+        )}
+        film_data['Название фильма'] = film_name.split("\n")
+
+        return film_data
