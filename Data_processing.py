ea5941c17c6dd8f5f1fedf986d34d0bded95b296
Asvad <asvadkhadchukaev@gmail.com>
2023-01-26 02:04:14 +0300

    small update

--- Data_processing.py
+++ Data_processing.py
@@ -14,29 +14,22 @@ class DataTransformation():
                 df):
         self.df = df
 
-    def __enter__(self):
-        print('Start data transformation')
-
-    def __exit__(self, exc_type, exc_value, ex_traceback):
-        print('End data transformation')
-    
     def dropping_columns(self, thresh_number=180):
         self.df_cleared = self.df.dropna(axis=1, thresh=thresh_number)
-        return self.df_cleared
 
 
     def replace_symbol_of_cell(self, cell, symbol_to_be_replaced, replacement):
         if type(cell) == str:
-            self.cell = cell.replace(symbol_to_be_replaced, replacement)
+            cell = cell.replace(symbol_to_be_replaced, replacement)
         else:
             cell = str(cell)
-            self.cell = cell.replace(symbol_to_be_replaced, replacement)
-        return self.cell
+            cell = cell.replace(symbol_to_be_replaced, replacement)
+        return cell
 
 
     def convert_to_currency(self, cell, currency_input, year, currency_required='USD'):
         converter = CurrencyConverter()
-        self.cell = (converter.convert(cell, currency_input, currency_required, date=date(year, 1, 1)))
+        cell = (converter.convert(cell, currency_input, currency_required, date=date(year, 1, 1)))
 
     def clear_column(self, column, replacement, to_be_replaced):
         self.df_cleared[column] = self.df_cleared[column].str.replace(to_be_replaced, replacement)
@@ -66,7 +59,50 @@ class DataTransformation():
         all_categories = list(set(all_categories))
         for category in all_categories:
             self.df_cleared[column + ': ' + category] = self.df_cleared[column].apply(lambda x: True if category in x else False)
-            
+
+    def _budget(self, x):
+        currencies_signs = ['€', 'DEM', 'р.', '₽', '£', 'FRF', '¥', 'DKK']
+        currencies = ['EUR', 'DEM', 'RUB', 'RUB', 'GBP', 'FRF', 'JPY', 'DKK']
+        try:
+            year = self.df_cleared.loc[self.df_cleared['Бюджет'] == x, 'Год производства'].values[0]
+            for currency in currencies_signs:
+                if currency in str(x):
+                    print(x)
+                    x = self.replace_symbol_of_cell(cell=x, symbol_to_be_replaced=currency, replacement='')
+                    print(x)
+                    x = float(x)
+                    x = self.convert_to_currency(cell=x, currency=currencies[currencies_signs.index(currency)],year = date(year, 1, 1))
+                else:
+                    continue
+            return x
+        except:
+            return x
+
+    def budget_column(self):
+        garbage = [' ', '$']
+        for element in garbage:
+            self.clear_column(column='Бюджет', replacement='', to_be_replaced=element)   
+        self.df_cleared['Бюджет'] = self.df_cleared['Бюджет'].apply(lambda y: self._budget(y))
+        self.df_cleared['Бюджет'] = self.df_cleared['Бюджет'].apply(pd.to_numeric)
+        
+
+    def categorical_columns(self):
+        columns = ['Жанр', 'Страна', 'Актеры', 'Режиссер', 'Сценарий']
+        for column in columns:
+            self.categorical_data_processing(column=column)
+        
+
+    def views_column(self):
+        self.df_cleared['Зрители'] = self.df_cleared['Зрители'].apply(lambda x: self.convert_to_float(x))
+
+    def fees_columns(self):
+        columns = ['Сборы в США', 'Сборы в мире']
+        garbage = [' ', '$']    
+        for column in columns:
+            for element in garbage:
+                self.clear_column(column=column, replacement='', to_be_replaced=element)
+            self.df_cleared[column] = self.df_cleared[column].apply(lambda x: max(x.split('=')) if type(x) == str else x)
+                
     
 
     
--- main.py
+++ main.py
@@ -36,53 +36,6 @@ def find_all_film_data(parser: KinoPoisk, all_links: list) -> list[dict]:
         all_film_data.append(film_data)
         logger.info(f'Got data from the {link_number+1} link film')
     return all_film_data
-
-def _budget(transformer: DataTransformation, x):
-    currencies_signs = ['€', 'DEM', 'р.', '₽', '£', 'FRF', '¥', 'DKK']
-    currencies = ['EUR', 'DEM', 'RUB', 'RUB', 'GBP', 'FRF', 'JPY', 'DKK']
-    try:
-        year = transformer.df_cleared.loc[transformer.df_cleared['Бюджет'] == x, 'Год производства'].values[0]
-        for currency in currencies_signs:
-            if currency in str(x):
-                print(x)
-                x = transformer.replace_symbol_of_cell(cell=x, symbol_to_be_replaced=currency, replacement='')
-                print(x)
-                x = float(x)
-                x = transformer.convert_to_currency(cell=x, currency=currencies[currencies_signs.index(currency)],year = date(year, 1, 1))
-            else:
-                continue
-        return x
-    except:
-        return x
-
-def budget_column(transformer: DataTransformation):
-    garbage = [' ', '$']
-    for element in garbage:
-        transformer.clear_column(column='Бюджет', replacement='', to_be_replaced=element)   
-    transformer.df_cleared['Бюджет'] = transformer.df_cleared['Бюджет'].apply(lambda y: _budget(transformer, x=y))
-    transformer.df_cleared['Бюджет'] = transformer.df_cleared['Бюджет'].apply(pd.to_numeric)
-    return transformer.df_cleared
-    
-
-def categorical_columns(transformer: DataTransformation):
-    columns = ['Жанр', 'Страна', 'Актеры', 'Режиссер', 'Сценарий']
-    for column in columns:
-        transformer.categorical_data_processing(column=column)
-    return transformer.df_cleared
-    
-
-def views_column(transformer: DataTransformation):
-    transformer.df_cleared['Зрители'] = transformer.df_cleared['Зрители'].apply(lambda x: transformer.convert_to_float(cell=x))
-    return transformer.df_cleared
-
-def fees_columns(transformer: DataTransformation):
-    columns = ['Сборы в США', 'Сборы в мире']
-    garbage = [' ', '$']    
-    for column in columns:
-        for element in garbage:
-            transformer.clear_column(column=column, replacement='', to_be_replaced=element)
-        transformer.df_cleared[column] = transformer.df_cleared[column].apply(lambda x: max(x.split('=')) if type(x) == str else x) 
-    return transformer.df_cleared
         
 def create_db_table(df):
     sqlite_file = 'KinoPoisk.db'
@@ -100,11 +53,11 @@ def main():
 
     transformer = DataTransformation(df)
     transformer.dropping_columns()
-    budget_column(transformer)
-    categorical_columns(transformer)
-    views_column(transformer)
-    df = fees_columns(transformer)
-    df.to_csv('KinoPoisk_processed', index=False)
+    transformer.budget_column()
+    transformer.categorical_columns()
+    transformer.views_column()
+    transformer.fees_columns()
+    transformer.df_cleared.to_csv('KinoPoisk_processed', index=False)
     create_db_table(df=df)
 
 if __name__ == '__main__':
