2e233e460445edd671c6e120ccc0dbabf24df796
Asvad <asvadkhadchukaev@gmail.com>
2023-03-03 00:20:13 +0300

    minor changes

--- Dashboard.py
+++ Dashboard.py
@@ -1,113 +1,169 @@
 from dash import Dash, html, dcc, callback
 from dash.dependencies import Input, Output
-from matplotlib import pyplot as plt
-from matplotlib import ticker as tick
-import numpy as np
-import seaborn as sns
 import pandas as pd
 import plotly.express as px
 from Vis_func import bar_plot
 from Vis_func import count_plot
 
+# Load data
 df = pd.read_csv('KinoPoisk_processed')
+
+# Initialize the app
 app = Dash()
 
+# Group data by year and find the maximum number of viewers per year
 viewers_per_year = df.groupby('Год производства')['Зрители'].max()
-years = [df['Год производства'].min(), 1950, 1960, 1970, 1980, 1990, 2000, 2010, df['Год производства'].max()]
-fig = px.line(viewers_per_year, title='Maximum number of viewers per year of production')
 
+# Define the years to display in the range slider and line plot
+years = [df['Год производства'].min(), 1950, 1960, 1970, 1980, 1990, 2000, 2010,
+         df['Год производства'].max()]
 
+# Create the line plot
+fig = px.line(viewers_per_year,
+              title='Maximum number of viewers per year of production')
+
+# Define the layout of the app
 app.layout = html.Div(children=[
-    html.Div([
-            html.H1(children='KinoPoisk Dashboard (top films)'),
-            dcc.RangeSlider(
-                min = df['Год производства'].min(),
-                max = df['Год производства'].max(),
-                id = 'slider_years',
-                marks = {int(year): {'label': str(year)} for year in years},
-                value = [df['Год производства'].min(), df['Год производства'].max()],
-                step=1,),
-            dcc.Graph(
-                id='viewers_per_year', 
-                figure=fig)]),
-    html.Div([
-            dcc.Dropdown(
-                ['Genres', 'Countries'], 
-                value = 'Genres',
-                id = 'dropdown_pie'),
-            dcc.Graph(
-                id = 'number_of_films', 
-                figure = count_plot(df = df, name_of_category_column='Жанр', title_name='The percentage of top films by genre'))]),
-            dcc.Dropdown(
-                ['Genres', 'Countries'], 
-                value = 'Genres',
-                id = 'dropdown_bar_category',
-                style={'width': '90vh', 'display': 'inline-block'}),
-            dcc.Dropdown(
-                ['Revenue', 'Viewers', 'Score'], 
-                value = 'Revenue',
-                id = 'dropdown_bar_measure',
-                style={'width': '90vh', 'display': 'inline-block'}),
-            dcc.Graph(
-                id = 'bar_plot',
-                figure = bar_plot(df = df, name_of_category_column = 'Жанр', name_of_measerment_column = 'Сборы в мире', title_name = 'The average Revenue of top films by genre'),
-            )])
+    html.H1(children='KinoPoisk Dashboard (top films)'),
 
-@callback(
+    html.Div([
+        dcc.RangeSlider(
+            min=df['Год производства'].min(),
+            max=df['Год производства'].max(),
+            id='slider_years',
+            marks={int(year): {'label': str(year)} for year in years},
+            value=[df['Год производства'].min(), df['Год производства'].max()],
+            step=1,
+            className='range-slider'
+        )
+    ], className='range-slider-container'),
+
+    html.Div([
+        dcc.Graph(
+            id='viewers_per_year',
+            figure=fig,
+            className='line-plot'
+        )
+    ], className='line-plot-container'),
+
+    html.Div([
+        dcc.Dropdown(
+            options=[{'label': 'Genres', 'value': 'Genres'},
+                     {'label': 'Countries', 'value': 'Countries'}],
+            value='Genres',
+            id='dropdown_pie',
+            className='dropdown'
+        )
+    ], className='dropdown-container'),
+
+    html.Div([
+        dcc.Graph(
+            id='number_of_films',
+            figure=count_plot(df=df,
+                              name_of_category_column='Жанр',
+                              title_name='The percentage of top films by genre'),
+            className='pie-chart'
+        )
+    ], className='pie-chart-container'),
+
+    html.Div([
+        dcc.Dropdown(
+            options=[{'label': 'Genres', 'value': 'Genres'},
+                     {'label': 'Countries', 'value': 'Countries'}],
+            value='Genres',
+            id='dropdown_bar_category',
+            className='dropdown-bar-category'
+        ),
+        dcc.Dropdown(
+            options=[{'label': 'Revenue', 'value': 'Revenue'},
+                     {'label': 'Viewers', 'value': 'Viewers'},
+                     {'label': 'Score', 'value': 'Score'}],
+            value='Revenue',
+            id='dropdown_bar_measure',
+            className='dropdown-bar-measure'
+        )
+    ], className='dropdown-bar-container'),
+
+    html.Div([
+        dcc.Graph(
+            id='bar_plot',
+            figure=bar_plot(df=df,
+                            name_of_category_column='Жанр',
+                            name_of_measerment_column='Сборы в мире',
+                            title_name='The average Revenue of top films by genre'),
+            className='bar-chart'
+        )
+    ], className='bar-chart-container')
+])
+
+# Define the callbacks
+
+
+@app.callback(
     Output('viewers_per_year', 'figure'),
     Input('slider_years', 'value')
 )
-
 def update_graph(years_list):
-    dff = df[(df['Год производства']>= years_list[0]) & (df['Год производства']<= years_list[1])]
+    dff = df[(df['Год производства'] >= years_list[0]) &
+             (df['Год производства'] <= years_list[1])]
     viewers_per_year = dff.groupby('Год производства')['Зрители'].max()
-    fig = px.line(viewers_per_year)
+    fig = px.line(viewers_per_year,
+                  title='Maximum number of viewers per year of production')
+    fig.update_layout(xaxis_title='Year',
+                      yaxis_title='Max viewers')
     return fig
 
-@callback(
+
+@app.callback(
     Output('number_of_films', 'figure'),
     Input('dropdown_pie', 'value')
 )
-
 def update_count_plot(category):
     if category == 'Genres':
-        cols = 'Жанр: '
+        category_column = 'Жанр'
         title_name = 'The percentage of top films by genre'
     else:
-        cols = 'Страна: '
+        category_column = 'Страна'
         title_name = 'The percentage of top films by country'
-    dictionary = {col.split(': ')[1]: df[df[col]==True]['Название фильма'].count() for col in df.columns if cols in col}
-    dictionary = dict(sorted(dictionary.items(), key=lambda x:x[1], reverse=True))
-    df_small = pd.melt(pd.DataFrame(dictionary, index=[0]))
-    df.small = df_small.loc[(df_small['value']/df_small['value'].sum())<0.04, 'variable'] = 'другие'
-    figure_count = px.pie(data_frame=df_small, names=df_small['variable'], values=df_small['value'], title=title_name)
-    return figure_count
+    figure = count_plot(df=df,
+                        name_of_category_column=category_column,
+                        title_name=title_name)
+    return figure
 
-@callback(
+
+@app.callback(
     Output('bar_plot', 'figure'),
     Input('dropdown_bar_category', 'value'),
     Input('dropdown_bar_measure', 'value')
 )
-
-def update_bar_plot(category, measure):
+def update_bar_plot(category,
+                    measure):
     if measure == 'Revenue':
         measure_col = 'Сборы в мире'
         title_name = 'The average Revenue of top films'
+        scale = None
     elif measure == 'Viewers':
         measure_col = 'Зрители'
         title_name = 'The average number of viewers of top films'
+        scale = None
     else:
         measure_col = 'Оценка фильма'
         title_name = 'The average score of top top films'
+        scale = [7.5, 9]
     if category == 'Genres':
         cat_col = 'Жанр'
         title_name = title_name + ' by genre'
     else:
         cat_col = 'Страна'
         title_name = title_name + ' by country'
-    fig = bar_plot(df= pd.read_csv('KinoPoisk_processed'), name_of_category_column=cat_col, name_of_measerment_column=measure_col, title_name=title_name)
+    fig = bar_plot(df=df,
+                   name_of_category_column=cat_col,
+                   name_of_measerment_column=measure_col,
+                   title_name=title_name,
+                   scale_of_axis=scale)
     return fig
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, use_reloader=False)
+    app.run_server(debug=True,
+                   use_reloader=False)
--- Vis_func.py
+++ Vis_func.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import numpy as np
 import plotly.express as px
 import re
 
 
-def reformat_large_tick_values(tick_val, pos):
+def reformat_large_tick_values(tick_val,
+                               pos):
     """
     Turns large tick values (in the billions, millions and thousands) such as 4500 into 4.5K and also appropriately turns 4000 into 4K (no zero after the decimal).
     """
@@ -24,40 +24,27 @@ def reformat_large_tick_values(tick_val, pos):
 
     # make new_tick_format into a string value
     new_tick_format = str(new_tick_format)
-    
+
     # code below will keep 4.5M as is but change values such as 4.0M to 4M since that zero after the decimal isn't needed
     index_of_decimal = new_tick_format.find(".")
-    
+
     if index_of_decimal != -1:
         value_after_decimal = new_tick_format[index_of_decimal+1]
         if value_after_decimal == "0":
             # remove the 0 after the decimal point since it's not needed
-            new_tick_format = new_tick_format[0:index_of_decimal] + new_tick_format[index_of_decimal+2:]
-            
+            new_tick_format = new_tick_format[0:index_of_decimal] + \
+                new_tick_format[index_of_decimal+2:]
+
     return new_tick_format
 
-def show_values(axs, orient="v", space=0):
-    def _single(ax):
-        if orient == "v":
-            for p in ax.patches:
-                _x = p.get_x() + p.get_width() / 2
-                _y = p.get_y() + p.get_height() + (p.get_height()*0.01)
-                value = reformat_large_tick_values(p.get_height(), 0)
-                ax.text(_x, _y, value, ha="center") 
-        elif orient == "h":
-            for p in ax.patches:
-                _x = p.get_x() + p.get_width() + float(space)
-                _y = p.get_y() + p.get_height() - (p.get_height()*0.5)
-                value = reformat_large_tick_values(p.get_width(), 0)
-                ax.text(_x, _y, value, ha="left")
 
-    if isinstance(axs, np.ndarray):
-        for idx, ax in np.ndenumerate(axs):
-            _single(ax)
-    else:
-        _single(axs)
+def bar_plot(df,
+             name_of_category_column,
+             name_of_measerment_column,
+             title_name,
+             aggregation_func='mean',
+             scale_of_axis=None):
 
-def bar_plot(df, name_of_category_column, name_of_measerment_column, title_name, aggregation_func='mean', scale_of_axis=None):
     all_categories = []
     for record in df[name_of_category_column]:
         new_record = record.split(',')
@@ -69,35 +56,37 @@ def bar_plot(df, name_of_category_column, name_of_measerment_column, title_name,
         measurement_by_category = {category: df[df[name_of_category_column].str.contains(re.escape(category))][name_of_measerment_column].mean()
                                    for category in all_categories}
     elif aggregation_func == 'median':
-        measurement_by_category = {category: df[df[name_of_category_column].str.contains(category)][name_of_measerment_column].median() 
+        measurement_by_category = {category: df[df[name_of_category_column].str.contains(category)][name_of_measerment_column].median()
                                    for category in all_categories}
-    dictionary = dict(sorted(measurement_by_category.items(), 
-                                key=lambda x:x[1], 
-                                reverse=False))
-    df_small = pd.melt(pd.DataFrame(dictionary, 
+    dictionary = dict(sorted(measurement_by_category.items(),
+                             key=lambda x: x[1],
+                             reverse=False))
+    df_small = pd.melt(pd.DataFrame(dictionary,
                                     index=[0]))
-    df_small.loc[(df_small['value']/df_small['value'].sum())<0.03, 'variable'] = 'другое'
-    df_small.loc[len(df_small.index)] = ['другие', df_small[df_small['variable']=='другое']['value'].sum()] 
-    df_small.drop(df_small[df_small['variable']=='другое'].index, 
+    df_small.loc[(df_small['value']/df_small['value'].sum())
+                 < 0.03, 'variable'] = 'другое'
+    df_small.loc[len(df_small.index)] = [
+        'другие', df_small[df_small['variable'] == 'другое']['value'].sum()]
+    df_small.drop(df_small[df_small['variable'] == 'другое'].index,
                   inplace=True)
-    df_small = df_small.sort_values(by=['value'], 
+    df_small = df_small.sort_values(by=['value'],
                                     ascending=True)
     fig = px.histogram(data_frame=df_small,
-                       y='variable', 
-                       x='value', 
-                       title=title_name, 
+                       y='variable',
+                       x='value',
+                       title=title_name,
                        text_auto='.2s')
-    fig.update_layout(barmode='stack', 
-                      xaxis={'categoryorder': 'total descending', 
-                             'visible': False, 
-                             'showticklabels' : False}, 
-                      yaxis={'visible': True, 
-                             'title' : None, 
-                             'showticklabels' : True},
+    fig.update_layout(barmode='stack',
+                      xaxis={'categoryorder': 'total descending',
+                             'visible': False,
+                             'showticklabels': False},
+                      yaxis={'visible': True,
+                             'title': None,
+                             'showticklabels': True},
                       xaxis_range=scale_of_axis,
                       width=600,
                       height=600,
-                      title={'x':0.5,
+                      title={'x': 0.5,
                              'xanchor': 'center'}
                       )
     return fig
@@ -112,23 +101,20 @@ def count_plot(df, name_of_category_column, title_name):
         all_categories.append(word)
     all_categories = list(set(all_categories))
     dictionary = {category: df[df[name_of_category_column].str.contains(re.escape(category))]['Название фильма'].count()
-              for category in all_categories}
-    dictionary = dict(sorted(dictionary.items(), 
-                             key=lambda x:x[1], 
+                  for category in all_categories}
+    dictionary = dict(sorted(dictionary.items(),
+                             key=lambda x: x[1],
                              reverse=True))
-    df_small = pd.melt(pd.DataFrame(dictionary, 
+    df_small = pd.melt(pd.DataFrame(dictionary,
                                     index=[0]))
-    df_small.loc[(df_small['value']/df_small['value'].sum())<0.04, 
+    df_small.loc[(df_small['value']/df_small['value'].sum()) < 0.04,
                  'variable'] = 'другие'
-    fig = px.pie(data_frame=df_small, 
-                 names=df_small['variable'], 
-                 values=df_small['value'], 
+    fig = px.pie(data_frame=df_small,
+                 names=df_small['variable'],
+                 values=df_small['value'],
                  title=title_name)
-    fig.update_layout(title={'x':0.5,
+    fig.update_layout(title={'x': 0.5,
                              'xanchor': 'center'},
                       width=500,
-                      height=450)
+                      height=500)
     return fig
-
-
-
